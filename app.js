fd1a8e338e7b9d3bc38c5497dfac2b27a9c03423
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-03 23:58:53 -0700

    Refactor functionality with promises and events

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
 
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const emitter = require('./event.js');
+require('./logger.js');
+
+/**
+ * Reads data from a file asynchronously
+ *
+ * @param {string} fileName
+ * @returns {Promise<Buffer>} - Promise object representing the file contents
+ */
+const fileRead = fileName => readFile(fileName);
+
+/**
+ * Converts a Buffer representing a string to uppercase
+ *
+ * @param {Buffer} data - Buffer representing a string
+ * @returns {Buffer} - The uppercase representation of the data
+ */
+const uppercase = data => Buffer.from(data.toString().toUpperCase());
+
+/**
+ * Writes data to a file asynchronously
+ *
+ * @param {string} fileName
+ * @param {Buffer} data - Buffer representing a string
+ * @returns {Promise}
+ */
+const fileSave = (fileName, data) => writeFile(fileName, data);
+
+/**
+ * Alters the given file by converting it to uppercase
+ *
+ * @param {string} fileName
+ * @fires 'file-altered'
+ * @fires 'file-error'
+ */
+const alterFile = fileName => {
+
+  fileRead(fileName)
+    .then(uppercase)
+    .then(data => fileSave(fileName, data))
+    .then(() => emitter.emit('file-altered', fileName))
+    .catch(error => emitter.emit('file-error', error));
+
 };
 
+
+// CLI Entry
+
 let file = process.argv.slice(2).shift();
-alterFile(file);
+
+if (file) {
+  alterFile(file);
+} else {
+  emitter.emit('file-error', Error('Expected argument: [filename]'));
+}
